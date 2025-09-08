caa21c58f99922122e92370aa5a6778d16447fd3
PhamTuanAnh <tuan.pham.21.01.2003@gmail.com>
2025-09-09 00:53:58 +0700

    4th commit

--- src/assets/4.wgsl
+++ src/assets/4.wgsl
@@ -40,35 +40,38 @@
 
 
 
+    struct Vertex
+//  struct Vertex
+{
+    position: vec2<f32>,
+//  position: vec2<f32>,
+};
+
+
+
     @group(0) @binding(0) var<storage, read> staticInputs: array<StaticInput>;
 //  @group(0) @binding(0) var<storage, read> staticInputs: array<StaticInput>;
     @group(0) @binding(1) var<storage, read> dynamicInputs: array<DynamicInput>;
 //  @group(0) @binding(1) var<storage, read> dynamicInputs: array<DynamicInput>;
+    @group(0) @binding(2) var<storage, read> vertices: array<Vertex>;
+//  @group(0) @binding(2) var<storage, read> vertices: array<Vertex>;
 
 
 
     @vertex fn vertexShader(@builtin(vertex_index) vertexIndex : u32, @builtin(instance_index) instanceIndex: u32) -> VertexShaderOutput
 //  @vertex fn vertexShader(@builtin(vertex_index) vertexIndex : u32, @builtin(instance_index) instanceIndex: u32) -> VertexShaderOutput
 {
-    let vertexPosition: array<vec2<f32>, 3> = array<vec2<f32>, 3>(
-//  let vertexPosition: array<vec2<f32>, 3> = array<vec2<f32>, 3>(
-        vec2<f32>( 0.0,  0.5),
-//      vec2<f32>( 0.0,  0.5),
-        vec2<f32>(-0.5, -0.5),
-//      vec2<f32>(-0.5, -0.5),
-        vec2<f32>( 0.5, -0.5),
-//      vec2<f32>( 0.5, -0.5),
-    );
-
     let staticInput: StaticInput = staticInputs[instanceIndex];
 //  let staticInput: StaticInput = staticInputs[instanceIndex];
     let dynamicInput: DynamicInput = dynamicInputs[instanceIndex];
 //  let dynamicInput: DynamicInput = dynamicInputs[instanceIndex];
+    let vertex: Vertex = vertices[vertexIndex];
+//  let vertex: Vertex = vertices[vertexIndex];
 
     var vertexShaderOutput: VertexShaderOutput;
 //  var vertexShaderOutput: VertexShaderOutput;
-    vertexShaderOutput.vertexPosition = vec4<f32>(vertexPosition[vertexIndex] * dynamicInput.scale + staticInput.offset, 0.0, 1.0);
-//  vertexShaderOutput.vertexPosition = vec4<f32>(vertexPosition[vertexIndex] * dynamicInput.scale + staticInput.offset, 0.0, 1.0);
+    vertexShaderOutput.vertexPosition = vec4<f32>(vertex.position * dynamicInput.scale + staticInput.offset, 0.0, 1.0);
+//  vertexShaderOutput.vertexPosition = vec4<f32>(vertex.position * dynamicInput.scale + staticInput.offset, 0.0, 1.0);
     vertexShaderOutput.color = staticInput.color;
 //  vertexShaderOutput.color = staticInput.color;
     return vertexShaderOutput;
--- src/lib/ExampleD.svelte
+++ src/lib/ExampleD.svelte
@@ -62,6 +62,12 @@
 //  let gpuBindGroup: GPUBindGroup;
     let dynamicStorageValues: Float32Array;
 //  let dynamicStorageValues: Float32Array;
+    type CreateCircleVerticesResult = { vertexData: Float32Array, numVertices: number, };
+//  type CreateCircleVerticesResult = { vertexData: Float32Array, numVertices: number, };
+    let createCircleVerticesResult: CreateCircleVerticesResult;
+//  let createCircleVerticesResult: CreateCircleVerticesResult;
+    let verticesStorageBuffer: GPUBuffer;
+//  let verticesStorageBuffer: GPUBuffer;
 
 
 
@@ -212,6 +218,19 @@
         }
         dynamicStorageValues = new Float32Array(dynamicStorageBufferSize / 4);
 //      dynamicStorageValues = new Float32Array(dynamicStorageBufferSize / 4);
+        createCircleVerticesResult = createCircleVertices({ radius: 0.5, innerRadius: 0.25, });
+//      createCircleVerticesResult = createCircleVertices({ radius: 0.5, innerRadius: 0.25, });
+        verticesStorageBuffer = gpuDevice.createBuffer({
+//      verticesStorageBuffer = gpuDevice.createBuffer({
+            label: "storage buffer vertices",
+//          label: "storage buffer vertices",
+            size: createCircleVerticesResult.vertexData.byteLength,
+//          size: createCircleVerticesResult.vertexData.byteLength,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+//          usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+        });
+        gpuDevice.queue.writeBuffer(verticesStorageBuffer, 0, createCircleVerticesResult.vertexData as GPUAllowSharedBufferSource);
+//      gpuDevice.queue.writeBuffer(verticesStorageBuffer, 0, createCircleVerticesResult.vertexData as GPUAllowSharedBufferSource);
         gpuBindGroup = gpuDevice.createBindGroup({
 //      gpuBindGroup = gpuDevice.createBindGroup({
             label: "bind group for objects",
@@ -221,10 +240,12 @@
             entries:
 //          entries:
             [
-                { binding: 0, resource: { buffer:  staticStorageBuffer, }, },
-//              { binding: 0, resource: { buffer:  staticStorageBuffer, }, },
-                { binding: 1, resource: { buffer: dynamicStorageBuffer, }, },
-//              { binding: 1, resource: { buffer: dynamicStorageBuffer, }, },
+                { binding: 0, resource: { buffer:   staticStorageBuffer, }, },
+//              { binding: 0, resource: { buffer:   staticStorageBuffer, }, },
+                { binding: 1, resource: { buffer:  dynamicStorageBuffer, }, },
+//              { binding: 1, resource: { buffer:  dynamicStorageBuffer, }, },
+                { binding: 2, resource: { buffer: verticesStorageBuffer, }, },
+//              { binding: 2, resource: { buffer: verticesStorageBuffer, }, },
             ]
             ,
         });
@@ -325,8 +346,8 @@
 //      gpuDevice.queue.writeBuffer(dynamicStorageBuffer, 0, dynamicStorageValues as GPUAllowSharedBufferSource);        
         gpuRenderPassEncoder.setBindGroup(0, gpuBindGroup);
 //      gpuRenderPassEncoder.setBindGroup(0, gpuBindGroup);
-        gpuRenderPassEncoder.draw(3, kNumObjects); // Call our vertex shader 3 times for each instance (3 vertices x 100 instances)
-//      gpuRenderPassEncoder.draw(3, kNumObjects); // Call our vertex shader 3 times for each instance (3 vertices x 100 instances)
+        gpuRenderPassEncoder.draw(createCircleVerticesResult.numVertices, kNumObjects); // Call our vertex shader numVertices times for each instance (numVertices vertices x 100 instances)
+//      gpuRenderPassEncoder.draw(createCircleVerticesResult.numVertices, kNumObjects); // Call our vertex shader numVertices times for each instance (numVertices vertices x 100 instances)
         gpuRenderPassEncoder.end();
 //      gpuRenderPassEncoder.end();
         gpuCommandBuffer = gpuCommandEncoder.finish();
@@ -366,7 +387,82 @@
         }
         return min + Math.random() * (max - min);
 //      return min + Math.random() * (max - min);
-};
+    };
+
+
+
+    const createCircleVertices = ({ radius = 1, numSubdivisions = 24, innerRadius = 0, startAngle = 0, endAngle = Math.PI * 2, } = {}) =>
+    {
+        // 2 triangles per subdivision, 3 verts per tri, 2 values (xy) each.
+        // 2 triangles per subdivision, 3 verts per tri, 2 values (xy) each.
+        const numVertices: number = numSubdivisions * 3 * 2;
+//      const numVertices: number = numSubdivisions * 3 * 2;
+        const vertexData: Float32Array = new Float32Array(numSubdivisions * 2 * 3 * 2);
+//      const vertexData: Float32Array = new Float32Array(numSubdivisions * 2 * 3 * 2);
+ 
+        let offset: number = 0;
+//      let offset: number = 0;
+        const addVertex = (x: number, y: number): void =>
+//      const addVertex = (x: number, y: number): void =>
+        {
+            vertexData[offset++] = x;
+//          vertexData[offset++] = x;
+            vertexData[offset++] = y;
+//          vertexData[offset++] = y;
+        };
+ 
+        // 2 triangles per subdivision
+        // 2 triangles per subdivision
+        //
+        //
+        // 0--1 4
+        // | / /|
+        // |/ / |
+        // 2 3--5
+        // 0--1 4
+        // | / /|
+        // |/ / |
+        // 2 3--5        
+        for (let i: number = 0; i < numSubdivisions; ++i)
+//      for (let i: number = 0; i < numSubdivisions; ++i)
+        {
+            const angle1: number = startAngle + (i + 0) * (endAngle - startAngle) / numSubdivisions;
+//          const angle1: number = startAngle + (i + 0) * (endAngle - startAngle) / numSubdivisions;
+            const angle2: number = startAngle + (i + 1) * (endAngle - startAngle) / numSubdivisions;
+//          const angle2: number = startAngle + (i + 1) * (endAngle - startAngle) / numSubdivisions;
+ 
+            const c1: number = Math.cos(angle1);
+//          const c1: number = Math.cos(angle1);
+            const s1: number = Math.sin(angle1);
+//          const s1: number = Math.sin(angle1);
+            const c2: number = Math.cos(angle2);
+//          const c2: number = Math.cos(angle2);
+            const s2: number = Math.sin(angle2);
+//          const s2: number = Math.sin(angle2);
+ 
+            // first triangle
+            // first triangle
+            addVertex(c1 *      radius, s1 *      radius);
+//          addVertex(c1 *      radius, s1 *      radius);
+            addVertex(c2 *      radius, s2 *      radius);
+//          addVertex(c2 *      radius, s2 *      radius);
+            addVertex(c1 * innerRadius, s1 * innerRadius);
+//          addVertex(c1 * innerRadius, s1 * innerRadius);
+ 
+            // second triangle
+            // second triangle
+            addVertex(c1 * innerRadius, s1 * innerRadius);
+//          addVertex(c1 * innerRadius, s1 * innerRadius);
+            addVertex(c2 *      radius, s2 *      radius);
+//          addVertex(c2 *      radius, s2 *      radius);
+            addVertex(c2 * innerRadius, s2 * innerRadius);
+//          addVertex(c2 * innerRadius, s2 * innerRadius);
+        }
+ 
+        return { vertexData, numVertices, };
+//      return { vertexData, numVertices, };
+    };
+
 
 
 </script>
